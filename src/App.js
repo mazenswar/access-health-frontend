e6424877705c707a0d8e4c81c5df941b99df107b
github username <github email address>
2019-04-13 19:16:53 -0400

    deleted useless handleDocMessage

--- src/App.js
+++ src/App.js
@@ -78,18 +78,13 @@ class App extends Component {
     this.props.history.push('/')
   }
 
-  handleDocMessage = (event) => {
-      event.preventsDefault()
-      console.log("clicked yo");
-  }
-
 
   render() {
 
     return (
       <div className="App">
         <Navbar handleLogout={this.handleLogout} user={this.state.currentUser}/>
-        <RouterComp handleDocMessage={this.handleDocMessage} handleLogin={this.handleLogin} handleSignup={this.handleSignup}/>
+        <RouterComp handleLogin={this.handleLogin} handleSignup={this.handleSignup}/>
         <Footer />
       </div>
     );
